e473cbf78033ce6b5ccd4cc129cb5a7006f460f0
Dimas Bagus Susilo <dimasbagussusilo@gmail.com>
2021-08-20 06:25:16 +0700

    Travis config

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const requireLogin = require("../middlewares/requireLogin");
+const cleanCache = require("../middlewares/cleanCache");
 
 const Blog = mongoose.model("Blog");
 
@@ -14,28 +15,14 @@ module.exports = (app) => {
   });
 
   app.get("/api/blogs", requireLogin, async (req, res) => {
-    // const redis = require("redis");
-    // const redisUrl = "redis://localhost:6379";
-    // const client = redis.createClient(redisUrl);
-    // const { promisify } = require("util");
-
-    // const getAsync = promisify(client.get).bind(client);
-    // const cachedBlogs = await getAsync(req.user.id);
-
-    // if (cachedBlogs) {
-    //   console.log("serving from catch", cachedBlogs);
-    //   return res.send(JSON.parse(cachedBlogs));
-    // }
-
-    const blogs = await Blog.find({ _user: req.user.id });
-
-    // client.set(req.user.id, JSON.stringify(blogs));
-    // console.log("serving from mongo");
+    const blogs = await Blog.find({ _user: req.user.id }).cache({
+      key: req.user.id,
+    });
 
     res.send(blogs);
   });
 
-  app.post("/api/blogs", requireLogin, async (req, res) => {
+  app.post("/api/blogs", requireLogin, cleanCache, async (req, res) => {
     const { title, content } = req.body;
 
     const blog = new Blog({
